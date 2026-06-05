808c9faf8154d1d555c09012967592d17be5ba9d
Victor Leal <victorlealdev@gmail.com>
2026-06-05 10:29:07 -0400

    feat: adiciona ATV05 e completa exercícios DIA1

--- src/main/java/com/victordev/DIA1/ATV05.java
+++ src/main/java/com/victordev/DIA1/ATV05.java
@@ -5,6 +5,22 @@ O sistema aceita no máximo 3 tentativas de senha. A senha correta é 1234. Simu
  */
 public class ATV05 {
     public static void main(String[] args) {
+        int senha = 1234;
+        boolean acertou = false;
+
+        for (int i = 1; i <= 3; i++) {
+            if (senha == 1234) {
+                acertou = true;
+                break;
+            }
+        }
+
+        if (acertou) {
+            System.out.println("Acesso liberado");
+        }
+        else {
+            System.out.println("Conta bloqueada");
+        }
 
     }
 }
